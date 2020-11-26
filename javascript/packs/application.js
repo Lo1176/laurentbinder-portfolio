a67d4776ac5f589f1e4a6cb61a9c1fdc270cffb6
Laurent Binder <laurentbinder@gmail.com>
2020-11-26 19:23:24 +0100

    scrollfunction js

--- javascript/packs/application.js
+++ javascript/packs/application.js
@@ -1,23 +1,48 @@
-require("@rails/ujs").start()
-require("@rails/activestorage").start()
-require("channels")
-// require("letterfx")
+// require("@rails/ujs").start()
+// require("@rails/activestorage").start()
+// require("channels")
 
-import "bootstrap";
+// import "bootstrap";
 
 // homemade plugins
-import initLetters from "../plugins/init_letters";
-// import initDetailsLetters from "../plugins/init_details_letters";
-import initSrollUp from "../plugins/init_scrollup";
-import initOpacity from "../plugins/init_opacity";
-import { init } from 'ityped'
-// import initItyped from "../plugins/init_ityped";
+// import initLetters from "../plugins/init_letters";
+// import initSrollUp from "../plugins/init_scrollup";
+// import initOpacity from "../plugins/init_opacity";
+// import { init } from 'ityped'
 
 
-// initDetailsLetters();
+// Wrap every letter in a span
+const initLetters = () => {
+  var textWrapper = document.querySelector('#mvlt .moving-letters .letters');
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+  anime.timeline({loop: false})
+    .add({
+      targets: '#mvlt .moving-letters .letter',
+      rotateY: [-90, 10],
+      duration: 6300,
+      delay: (el, i) => 45 * i
+    }).add({
+      targets: '#mvlt .moving-letters',
+      opacity: 1,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
+    });
+}
+
 initLetters();
-initSrollUp();
-initOpacity();
+
+// When the user scrolls down 400px from the top of the document, it shows the scroll button
+window.onscroll = scrollFunction;
+function scrollFunction() {
+  const state = document.documentElement.scrollTop > 400;
+  document.querySelector('.scrollup').style.display = state ? 'block' : 'none';
+};
+
+scrollFunction();
+
+// initOpacity();
 
 // letterfx is not working
 // $("#tagline").letterfx({"fx":"fly-right fly-bottom spin"});
@@ -29,36 +54,15 @@ const devElement = document.querySelector('#dev')
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+const initItyped = () => {
+  const devElement = document.querySelector('#dev')
+
+  init(devElement, { showCursor: true, startDelay: 20, typeSpeed:  90, backDelay:  500, disableBackTyping: false, strings: ['Création de sites Web modernes ', 'sur mesure', 'responsive', 'référencés', 'Magnifico !' ] });
+}
 async function firstSlogan(){
   await sleep(1000);
   init(devElement, { loop: false, typeSpeed:  90, startDelay: 1000, backDelay:  1500, disableBackTyping: false, strings: [ 'modernes', 'sur mesure', 'responsive', 'référencés', 'magnifico !', 'sur mesure' ]});
 };
 
-// setTimeout(function suffixSlogan() {
-//      init(devSuffixElement, { loop: false, typeSpeed:  90, backDelay:  500, disableBackTyping: false, strings: [ 'modernes', 'sur mesure', 'responsive', 'référencés', 'magnifico !', 'sur mesure ;)' ] });
-
-//   }, 8000);
 
 firstSlogan();
-
-// ---- function for hidding #navbar ----
-// $.fn.followTo = function (pos) {
-//     var $this = this,
-//         $window = $(window);
-
-//     $window.scroll(function (e) {
-//         if ($window.scrollTop() > pos) {
-//             $this.css({
-//                 position: 'absolute',
-//                 top: pos
-//             });
-//         } else {
-//             $this.css({
-//                 position: 'fixed',
-//                 top: 0
-//             });
-//         }
-//     });
-// };
-
-// $('#navbar').followTo(250);
