97876984a95e6724fc7d7bd309b00acd70acc3e4
Laurent Binder <laurentbinder@gmail.com>
2020-12-02 08:27:38 +0100

    deleted js plugins files

--- javascript/plugins/init_opacity.js
+++
@@ -1,9 +0,0 @@
-const initOpacity = () => {
-  $(document).ready(function(){
-    $(window).scroll(function(){
-      $('#ban').css("opacity", 1 - $(window).scrollTop()/400)
-    })
-  })
-};
-
-export default initOpacity;
